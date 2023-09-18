77760114b4c1f203decc515c6b05f379cfa5a91b
jjustindoesstats <98935997+jjustindoesstats@users.noreply.github.com>
2023-09-18 12:35:57 -0400

    Update Data_Simulation_Algorithm.py
    Added the color tracking list

--- Data_Simulation_Algorithm.py
+++ Data_Simulation_Algorithm.py
@@ -3,7 +3,7 @@ import Data_Simulation_Code as DSC
 import statistics as stat
 
 
-def Data_Simulation_Algorithm(n , M , T , prob , beta):
+def Data_Simulation_Algorithm(n , M , T , prob,beta):
     """
     
 
@@ -29,19 +29,18 @@ def Data_Simulation_Algorithm(n , M , T , prob , beta):
     distribution of balances at each period (min balance , max balance , mean, s.d , balance_data_red , balance_data_blue)
 
     """
-    #First check the conditions on n
-     #make sure conditions are satisfied
+    #Check the conditions of n
     if n % 2 != 0:
         return "n is not even"
     
     if n < 4:
         return "n is not greater then 4"
     
+    
     #First translate the period T into a list that can be looped over
     T_list = []
     for i in range(1 , T+1):
         T_list.append(i)
-
     
     #Intialize empty lists
     min_balance = []
@@ -49,11 +48,15 @@ def Data_Simulation_Algorithm(n , M , T , prob , beta):
     mean = []
     sd = []
     balance_data = []
+    color_data = []
     #Given the first period, we need to intialize the players values
     
     for i in T_list:
         if i == 1:
+            
             player_dict = DSC.intialization(n , M)
+            
+            
     
     #If the period is anytime after the first, activate the Color Partition step
         else:
@@ -76,18 +79,24 @@ def Data_Simulation_Algorithm(n , M , T , prob , beta):
     #Produce the required statistics
     #Create list for balance
         balance_list = []
+        color_list = []
+
+        
         for i in range(1 , n+1):
-            balance_list.append(player_dict[i]['Balance']) 
+            balance_list.append(player_dict[i]['Balance'])
+            color_list.append(player_dict[i]['Color']) 
+            
         
         min_balance.append(min(balance_list))
         max_balance.append(max(balance_list))
         mean.append(stat.mean(balance_list))
         sd.append(stat.stdev(balance_list))
         balance_data.append(balance_list)
+        color_data.append(color_list)
 
-    return min_balance , max_balance , mean , sd , balance_data
+
+    return min_balance , max_balance , mean , sd , balance_data , color_data
         
-
 
 
 
